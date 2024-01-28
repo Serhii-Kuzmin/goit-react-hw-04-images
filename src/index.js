455f3b38b3790572629427745a2bf8f9d37fec11
Serhii-Kuzmin <142728225+Serhii-Kuzmin@users.noreply.github.com>
2024-01-28 19:40:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter basename="/image-finder-refactoring-react-hooks">
-  <App />
-</BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </React.StrictMode>
 );
